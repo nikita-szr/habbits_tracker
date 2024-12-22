1089c57ab39ffd06af5aa36164ef9046f6f4bce4
nikitt <nikittaosipov@yandex.ru>
2024-12-22 20:21:59 +0400

    users urls

---
+++ users/urls.py
@@ -0,0 +1,27 @@
+from users.apps import UsersConfig
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from users.views import (
+    UserCreateView, UserViewSet,
+    MyTokenObtainPairView, MyTokenRefreshView
+)
+
+
+app_name = UsersConfig.name
+
+router = DefaultRouter()
+router.register(r'profile', UserViewSet)  # ----- профиль -----
+
+urlpatterns = [
+                  path('', include(router.urls)),
+
+                  # ----- регистрация и токен -----
+                  path('register/', UserCreateView.as_view(), name='register_api'),
+                  path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('token-refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
